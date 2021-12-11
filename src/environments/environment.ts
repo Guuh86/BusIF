8eb290f071f4d0d93983561ef0a5ab11e5411692
Luiz Gustavo <42076223+Guuh86@users.noreply.github.com>
2021-12-11 16:12:33 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDwTfAQCmOC4AAktZydDchN07h1y47nYi0",
-    authDomain: "busif-7c277.firebaseapp.com",
-    databaseURL: "https://busif-7c277-default-rtdb.firebaseio.com",
-    projectId: "busif-7c277",
-    storageBucket: "busif-7c277.appspot.com",
-    messagingSenderId: "611590824958",
-    appId: "1:611590824958:web:dbe82a77aae266114d96af",
-    measurementId: "G-V0SF12RWQT"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
